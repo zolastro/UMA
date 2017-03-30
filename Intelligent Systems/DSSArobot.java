24317345be62add535bcebb1ba6c47b97a5eeabf
David Ponce <zolastrodavid@gmail.com>
2017-03-30 12:53:43 +0200

    Implemented A*
    Still have to add the movement function

--- Intelligent Systems/DSSArobot.java
+++ Intelligent Systems/DSSArobot.java
@@ -6,15 +6,27 @@ import robocode.Robot;
 public class DSSArobot extends Robot {
 
 	private static final int NumColsRows = 15;
+	private static final int SEED = 27;
+	private static final int NumObstacles = 50;
 	private Spot positions[][] = new Spot[NumColsRows][NumColsRows];
+	
+	public static void main(String[] args) {
+		DSSArobot r = new DSSArobot();
+	}
+	
+	public DSSArobot(){
+		run();
+	}
 	
 	public void run(){
 		getMatrix(); 
 		List<Spot> path = AStarPathFinding();
+		System.out.println(path);
 		//TODO follow path
 	}
 	
 	private List<Spot> AStarPathFinding(){
+		List<Spot> path = new ArrayList<>();
 		Spot start = positions[0][0];
 		start.isWall = false;
 		Spot end = positions[NumColsRows-1][NumColsRows-1];
@@ -33,18 +45,61 @@ public class DSSArobot extends Robot {
 					bestSoFar = i;
 				}
 			}
-			Spot current = openList.get(bestSoFar);
-			if(current.equals(end)){
-				// End
+			Spot bestSpot = openList.get(bestSoFar);
+			if(bestSpot.equals(end)){
+				// We've finished!
+				path.add(bestSpot);
+				while(bestSpot.previous != null){
+					path.add(bestSpot.previous);
+					bestSpot = bestSpot.previous;
+				}
+				path = reverse(path);
+				return path;
 			}
+			openList.remove(bestSpot);
+			closeList.add(bestSpot);
+			List<Spot> neighbors = bestSpot.neigbors;
+			
+			for(int i = 0; i < neighbors.size(); i++){
+				if(!closeList.contains(neighbors.get(i)) && !neighbors.get(i).isWall){
+					int tempG = bestSpot.g + 1;		// Always cost 1 to move to the next position
+					boolean isNewPath = false;
+					if(openList.contains(neighbors.get(i))){
+						if(tempG < neighbors.get(i).g){
+							neighbors.get(i).g = tempG;		// New best path!
+							isNewPath = true;
+						}
+					}else{
+						neighbors.get(i).g = tempG;
+						isNewPath = true;
+						openList.add(neighbors.get(i));
+					}
+					if(isNewPath){
+						//Update the g and h values
+						neighbors.get(i).h = heuristic(neighbors.get(i), end);
+						neighbors.get(i).previous = bestSpot;
+					}
+				}
+			}	
 		}
-		
 		return null;
 	}	
 	
+	private List<Spot> reverse(List<Spot> original){
+		List<Spot> ans = new ArrayList<>();
+		for(int i = original.size()-1; i >= 0; i--){
+			ans.add(original.get(i));
+		}
+		return ans;
+	}
+ 	
+	private int heuristic(Spot origin, Spot target){
+		return Math.abs(origin.x - target.x) + Math.abs(origin.y - target.y);
+	}
+	
 	private void getMatrix(){
-		Random rand = new Random(5);
-		int NumObstacles = 20;
+		Random rand = new Random(SEED);
+		
 		int NdxObstacle = 0;
 		
 		for (int i = 0; i < NumColsRows; i++) {
@@ -57,14 +112,29 @@ public class DSSArobot extends Robot {
 		while (NdxObstacle<NumObstacles) {
 			i = rand.nextInt(NumColsRows-1);
 			j = rand.nextInt(NumColsRows-1);
-			System.out.println(i + " " + j);
 			if (!positions[i][j].isWall) {
 				positions[i][j].isWall = true;
 				NdxObstacle++;
 			}			
 		}
+		
+		for (int m = 0; m < NumColsRows; m++) {
+			for (int n = 0; n < NumColsRows; n++) {
+				positions[m][n].addNeighbors(positions, NumColsRows-1, NumColsRows-1);
+			}
+		}
+		showMap();
 	}
 	
+	
+	private void showMap(){
+		for (int i = 0; i < NumColsRows; i++) {
+			for (int j = 0; j < NumColsRows; j++) {
+				System.out.print((positions[i][j].isWall)? "*":"-");
+			}
+			System.out.println();
+		}
+	}
 	
 	
 }
--- Intelligent Systems/Spot.java
+++ Intelligent Systems/Spot.java
@@ -1,16 +1,17 @@
 package prRobots;
 
+import java.util.*;
+
 public class Spot {
 	public int x;
 	public int y;
 	public int g;
 	public int h;
 	public boolean isWall;
-	
 	public Spot previous;
-	
-	
-	public Spot(int i, int j){
+	public List<Spot> neigbors;
+
+	public Spot(int i, int j) {
 		x = i;
 		y = j;
 		g = 0;
@@ -18,12 +19,32 @@ public class Spot {
 		isWall = false;
 	}
 
+	public void addNeighbors(Spot[][] grid, int xLim, int yLim) {
+		neigbors = new ArrayList<>();
+		if (x < xLim) {
+			neigbors.add(grid[x + 1][y]);
+		}
+		if (x > 0) {
+			neigbors.add(grid[x -1][y]);
+		}
+		if (y < yLim) {
+			neigbors.add(grid[x][y+1]);
+		}
+		if (y > 0) {
+			neigbors.add(grid[x][y-1]);
+		}
+	}
+
 	@Override
 	public boolean equals(Object obj) {
-		if(obj instanceof Spot){
+		if (obj instanceof Spot) {
 			Spot spt = (Spot) obj;
 			return spt.x == this.x && spt.y == this.y;
 		}
 		return false;
+	}
+	
+	public String toString(){
+		return "(" + x + ", " + y + ")";
 	}
 }
